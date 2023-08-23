bc7493c4765367cb25a689dffcb78ffc63e2a827
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-08-23 11:05:49 +0700

    handle req get_started button

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -120,6 +120,8 @@ let handlePostback = async (sender_psid, received_postback) => {
     response = { text: "Thanks!" };
   } else if (payload === "no") {
     response = { text: "Oops, try sending another image." };
+  } else if (payload === "GET_STARTED") {
+    response = { text: "Welcome you to VAM Nguyen Restaurant!" };
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
